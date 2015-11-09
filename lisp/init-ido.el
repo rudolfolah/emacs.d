a827f8477471fe54702070ada29a7c1283e25547
Rudolf Olah <omouse@gmail.com>
2015-11-09 12:03:29 -0500

    using ido-mode only for buffer
    I prefer the standard behaviour for finding files and using projectile
    or speedbar if I'm searching for files

--- lisp/init-ido.el
+++ lisp/init-ido.el
@@ -1,6 +1,6 @@
 ;; Use C-f during file selection to switch to regular find-file
-(ido-mode t)
 (ido-everywhere t)
+(ido-mode 'buffers)
 (setq ido-enable-flex-matching t)
 (setq ido-use-filename-at-point nil)
 (setq ido-auto-merge-work-directories-length 0)
