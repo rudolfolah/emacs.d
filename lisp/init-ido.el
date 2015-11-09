e9d621ee879b2b340228751f4b52edeb87ee3d36
Rudolf Olah <omouse@gmail.com>
2015-11-09 12:04:12 -0500

    disabling ido-everywhere

--- lisp/init-ido.el
+++ lisp/init-ido.el
@@ -1,6 +1,6 @@
 ;; Use C-f during file selection to switch to regular find-file
-(ido-everywhere t)
 (ido-mode 'buffers)
+(ido-everywhere -1)
 (setq ido-enable-flex-matching t)
 (setq ido-use-filename-at-point nil)
 (setq ido-auto-merge-work-directories-length 0)
