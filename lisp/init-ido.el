54f62a4468f1393341797e610bb3b5cec45f2ebe
Rudolf Olah <omouse@gmail.com>
2015-11-09 12:04:24 -0500

    disabling ido virtual buffers
    virtual buffers are a list of previously opened files/buffers. this is
    somewhat useful but as mentioned in a previous commit, I usually use
    projectile or speedbar to navigate through files.

--- lisp/init-ido.el
+++ lisp/init-ido.el
@@ -4,7 +4,7 @@
 (setq ido-enable-flex-matching t)
 (setq ido-use-filename-at-point nil)
 (setq ido-auto-merge-work-directories-length 0)
-(setq ido-use-virtual-buffers t)
+(setq ido-use-virtual-buffers nil)
 
 (when (maybe-require-package 'ido-ubiquitous)
   (ido-ubiquitous-mode t))
